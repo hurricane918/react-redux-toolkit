e54c7dd6e0f26f4949badfe2524ccffbaca24669
hurricane918 <hurricaneninja918@gmail.com>
2022-01-29 21:50:12 -0500

    init commit

---
+++ src/components/couter.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Counter: React.FC = () => {
+  return (
+    <div>
+      <button>+</button>
+      <button>-</button>
+    </div>
+  );
+};
+
+export default Counter;
